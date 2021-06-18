889ba1f3c81b3ad15e673f4d019b9cca84187b09
= <wagner.lemos@ses.ba>
2021-06-18 16:10:43 -0300

    metodo que importa dados do animal na tabela correspondete especie e raca

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -88,7 +88,61 @@ class TarefaController extends Controller
        
     }  
 
-    
+    public function importacaoAnimal($request){
+        $caminho = $request->file('import')->path();     
+        $stream = fopen($caminho, 'r');
+        $csv = Reader::createFromStream($stream);
+        $csv->setDelimiter(",");
+        $csv->setHeaderOffset(0);
+
+       $stmt = (new Statement());
+
+       $dados = $stmt->process($csv);
+       
+       foreach($dados as $dado){
+
+            try{
+                $nascimento =  $dado['Nascimento'];
+            }catch(\Exception $e){
+                echo "Experado dados de Animal";
+                exit;
+            }
+
+            
+           
+            if($nascimento != null || !empty($nascimento)){  
+                if(!$this->validateDate($nascimento, 'Y-m-d')){
+                   $nascimento = \DateTime::createFromFormat('d/m/Y', $nascimento);
+               }
+            }else{
+                $nascimento =  null;
+            }
+           
+
+            $especie_id = Especie::firstOrCreate(
+            ['nome' => $dado['Especie'],],
+            );
+
+            $raca_id = Raca::create([
+                'especie_id' => $especie_id->id,
+                'nome' => $dado['Raca'],
+            ]);
+
+            $pessoa = Pessoa::where('id',$dado['IdCliente'])->value('id');
+
+            if($pessoa != NULL){
+
+                AnimalNovo::create([
+                    'pessoa_id' => $dado['IdCliente'],
+                    'especie_id' => $especie_id->id,
+                    'raca_id' => $raca_id->id,
+                    'nome' => $dado['Nome'],
+                    'nascimento' => $nascimento,
+                ]);
+            }
+       }
+        echo ("importou Animal");
+    }
     
     
 
